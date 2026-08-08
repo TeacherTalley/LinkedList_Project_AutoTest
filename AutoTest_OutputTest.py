41e3eaa54165da1bb1f66b0a45795633fb8d8d94
Terry Talley <ttalley@cub.uca.edu>
2026-08-08 18:13:44 -0500

    attempt to force bash as shell for diff and sed

--- AutoTest_OutputTest.py
+++ AutoTest_OutputTest.py
@@ -117,6 +117,7 @@ def diff_command(file1, file2, fixed_diff=True):
     """
     cmd = f'{DIFF} {file1} {file2}'
     if fixed_diff:
+        cmd = 'bash -c ' + cmd
         cmd = 'rc=$(' + cmd + f' | {FIXED_DIFF}' + '; exit ${PIPESTATUS[0]}); exit $rc'
 #        return f'rc = $({DIFF} {FIXED_DIFF}; exit ${PIPESTATUS[0]}); exit $rc'
         print(f'Crazy diff command: {cmd}')
